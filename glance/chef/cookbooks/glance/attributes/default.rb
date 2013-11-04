f498039393a4962944130aa10200bdb4d1377926
root <root@crowbaradmin.dub.com>
2013-11-04 12:04:52 +0000

    Add glance sync image functionality

--- glance/chef/cookbooks/glance/attributes/default.rb
+++ glance/chef/cookbooks/glance/attributes/default.rb
@@ -17,7 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+default[:glance][:rsync_user] = "rcuser"
 override[:glance][:user]="glance"
 
 default[:glance][:verbose] = "False"
