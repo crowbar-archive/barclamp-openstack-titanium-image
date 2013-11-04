28ec6b49ebcfb1df4eb05f7670eaef8b7320421c
root <root@crowbaradmin.dub.com>
2013-11-04 11:33:52 +0000

    Change cloud image URL

--- glance/setup.sh
+++ glance/setup.sh
@@ -2,5 +2,5 @@
 # Temporary cloud image download 
 mkdir -p cache/files/ami/
 echo "Downloading cloud image for Glance..."
-wget -q http://uec-images.ubuntu.com/releases/12.04.2/release/ubuntu-12.04-server-cloudimg-amd64.tar.gz -P cache/files/ami 
+wget -q http://uec-images.ubuntu.com/releases/12.04.2/release-20131024/ubuntu-12.04-server-cloudimg-amd64.tar.gz -P cache/files/ami 
 echo "Cloud image downloaded" 
